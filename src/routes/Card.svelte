2ac11d425310f06be428aa1063caae63380bd888
meiserloh <matthias.eiserloh@cloudogu.com>
2023-06-10 14:18:58 +0200

    delete unnecessary components

--- src/routes/Card.svelte
+++ src/routes/Card.svelte
@@ -1,5 +1,5 @@
 <div
-  class="h-full md:aspect-[4/5] m-20 px-4 py-10 bg-white shadow-lg rounded-3xl p-20 bg-clip-padding bg-opacity-20"
+  class="h-full md:aspect-[4/5] my-20 mx-2 px-4 py-10 bg-white shadow-lg rounded-3xl p-20 bg-clip-padding bg-opacity-20"
   style="backdrop-filter: blur(20px);">
   <slot/>
 </div>
