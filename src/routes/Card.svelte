df7c225214c23df223f41142ec9a0df457cc0397
meiserloh <matthias.eiserloh@cloudogu.com>
2023-06-13 07:05:24 +0200

    fix desktop width

--- src/routes/Card.svelte
+++ src/routes/Card.svelte
@@ -5,7 +5,7 @@
 
 <div in:fly="{{duration: ANIM_DURATION, x: 1000 * $animXFactor, y: 1000 * $animYFactor}}"
      out:fly="{{duration: ANIM_DURATION, x: -1000 * $animXFactor, y: -1000 * $animYFactor}}"
-     class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-4/5 md:aspect-[4/5] w-4/5 px-4 py-10 bg-white shadow-lg rounded-3xl bg-clip-padding bg-opacity-20"
+     class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-4/5 md:aspect-[4/5] w-4/5 md:w-auto px-4 py-10 bg-white shadow-lg rounded-3xl bg-clip-padding bg-opacity-20"
      style="backdrop-filter: blur(20px);">
   <slot />
 </div>
