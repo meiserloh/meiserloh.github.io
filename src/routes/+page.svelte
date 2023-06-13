e9ad939b061ea6dbaf8a6fa0e2b0be880f05072b
meiserloh <matthias.eiserloh@cloudogu.com>
2023-06-13 06:12:50 +0200

    add gestures for mobile usage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,11 +7,14 @@
   import Dev2 from "./Dev2.svelte";
   import Welcome from "./Welcome.svelte";
   import { animXFactor, animYFactor } from "./store.js";
+  import { swipe } from "svelte-gestures";
+  import Contact from "./Contact.svelte";
 
   let cardArray = [
     [{ component: Welcome }],
     [{ component: AboutMe1 }, { component: AboutMe2 }, { component: AboutMe3 }],
-    [{ component: Dev1 }, { component: Dev2 }]
+    [{ component: Dev1 }, { component: Dev2 }],
+    [{ component: Contact }],
   ];
 
   let xIndex = 0;
@@ -19,25 +22,36 @@
   $: selected = cardArray[xIndex][yIndex];
 
   function onKeyDown(e) {
-    console.log("x: " + xIndex, "y: " + yIndex);
-    switch (e.keyCode) {
-      case 38: // up
+    // console.log("x: " + xIndex, "y: " + yIndex);
+    let keyCodeToDirection = {37: "right", 38: "bottom", 39: "left", 40: "top"};
+    handleInputForSectionChange(keyCodeToDirection[e.keyCode]);
+  }
+
+  function handler(event) {
+    let direction = event.detail.direction;
+    // console.log(direction);
+    handleInputForSectionChange(direction);
+  }
+
+  function handleInputForSectionChange(animDirectionTo) {
+    switch (animDirectionTo) {
+      case "bottom":
         yIndex = Math.max(yIndex - 1, 0);
         animXFactor.set(0);
         animYFactor.set(-1);
         break;
-      case 40: // down
+      case "top":
         yIndex = Math.min(yIndex + 1, cardArray[xIndex].length - 1);
         animXFactor.set(0);
         animYFactor.set(1);
         break;
-      case 37: // left
+      case "right":
         xIndex = Math.max(xIndex - 1, 0);
         yIndex = 0;
         animXFactor.set(-1);
         animYFactor.set(0);
         break;
-      case 39: // right
+      case "left":
         xIndex = Math.min(xIndex + 1, cardArray.length - 1);
         yIndex = 0;
         animXFactor.set(1);
@@ -58,4 +72,5 @@
   <svelte:component this={selected.component} />
 </section>
 
+<svelte:body use:swipe={{ timeframe: 300, minSwipeDistance: 60}} on:swipe={handler} />
 <svelte:window on:keydown|preventDefault={onKeyDown} />
---
+++ src/routes/Contact.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { fly } from "svelte/transition";
+  import { animXFactor, animYFactor } from "./store.js";
+</script>
+<div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+     in:fly="{{duration: 750, x: 1000 * $animXFactor, y: 1000 * $animYFactor}}"
+     out:fly="{{duration: 750, x: -1000 * $animXFactor, y: -1000 * $animYFactor}}">
+  <h1 class="text-5xl text-white text-center">Contact</h1>
+</div>
