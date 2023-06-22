11cb04924630e85eefffc66d08665aee8fc93d3d
meiserloh <matthias.eiserloh@cloudogu.com>
2023-06-22 10:07:32 +0200

    Add arrow with animation

--- src/routes/Welcome.svelte
+++ src/routes/Welcome.svelte
@@ -1,12 +1,18 @@
 <script>
 	import { fly } from 'svelte/transition';
 	import { ANIM_DURATION, animXFactor, animYFactor } from './store.js';
+	import arrow from '$lib/images/arrow_right_blur.png';
 </script>
 
 <div
-	class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform"
+	class="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-center"
 	in:fly={{ duration: ANIM_DURATION, x: 1000 * $animXFactor, y: 1000 * $animYFactor }}
 	out:fly={{ duration: ANIM_DURATION, x: -1000 * $animXFactor, y: -1000 * $animYFactor }}
 >
 	<h1 class="text-center text-5xl text-white">MATTHIAS<br />EISERLOH</h1>
+	<img
+		src={arrow}
+		alt="arrow right"
+		class="relative top-32 -translate-x-4 animate-bounce-right-fade-in opacity-0"
+	/>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,36 @@ module.exports = {
 		require('path').join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
 	],
 	theme: {
-		extend: {}
+		extend: {
+			keyframes: {
+				bounceright: {
+					'0%': {
+						transform: 'none',
+						'animation-timing-function': 'cubic-bezier(.8,0,1,1)'
+					},
+					'50%': {
+						transform: 'translateX(1rem)',
+						'animation-timing-function': 'cubic-bezier(0,0,.2,1)'
+					},
+					'100%': {
+						transform: 'none',
+						'animation-timing-function': 'cubic-bezier(.8,0,1,1)'
+					}
+				},
+				fadeIn: {
+					'0%': {
+						opacity: '0%'
+					},
+					'100%': {
+						opacity: '100%'
+					}
+				}
+			},
+			animation: {
+				'bounce-right': 'bounceright 1.2s infinite',
+				'bounce-right-fade-in': 'fadeIn 2s 5s forwards, bounceright 1.2s infinite'
+			}
+		}
 	},
 	plugins: [
 		// 3. Append the Skeleton plugin to the end of this list
